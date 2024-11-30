39d64b24b0a7dfe5ca2af2e721384912927ce350
Im-Pranshu <pranshu.sharma1303@gmail.com>
2024-11-30 20:25:26 +0530

    bTree updated

--- C++Love/BTree/bTree.cpp
+++ C++Love/BTree/bTree.cpp
@@ -149,34 +149,78 @@ void buildFromLevelOrder(node *&root)
             q.push(temp->right);
         }
     }
-}
+};
+
+int i = 0;
+
+bool isIdentical(node *r1, node *r2)
+{
+    cout << "call " << ++i << endl;
+    // base case
+    if (r1 == NULL && r2 == NULL)
+    {
+        return true;
+    }
+
+    if (r1 == NULL && r2 != NULL)
+    {
+        return false;
+    }
+
+    if (r1 != NULL && r2 == NULL)
+    {
+        return false;
+    }
+
+    bool left = isIdentical(r1->left, r2->left);
+    // cout << "call" << endl;
+    bool right = isIdentical(r1->right, r2->right);
+
+    bool value = r1->data == r2->data;
+
+    if (left && right && value)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
 
 int main()
 {
 
-    node *root = NULL;
+    node *r1 = NULL;
+    node *r2 = NULL;
 
-    // buildFromLevelOrder(root);
+    buildFromLevelOrder(r1);
+    // 1 2 3 -1 -1 -1 -1
+    buildFromLevelOrder(r2);
+    // 1 2 3 -1 -1 -1 -1
+
+    cout << isIdentical(r1, r2);
+
     // levelOrderTraversal(root);
     // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
 
     // creating a Tree
-    root = buildTree(root);
+    // root = buildTree(root);
     // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     // level order
-    cout << "Printing the level order traversal output " << endl;
-    levelOrderTraversal(root);
+    // cout << "Printing the level order traversal output " << endl;
+    // levelOrderTraversal(root);
 
-    cout << "inorder traversal is:  ";
-    inorder(root);
+    // cout << "inorder traversal is:  ";
+    // inorder(root);
 
-    cout << endl
-         << "preorder traversal is:  ";
-    preorder(root);
+    // cout << endl
+    //      << "preorder traversal is:  ";
+    // preorder(root);
 
-    cout << endl
-         << "postorder traversal is:  ";
-    postorder(root);
+    // cout << endl
+    //      << "postorder traversal is:  ";
+    // postorder(root);
 
     return 0;
 }
